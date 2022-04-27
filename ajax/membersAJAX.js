b3e9f6b4fc61c53f6788026b1b9d75e123f5b295
abmysong <abmysong@gmail.com>
2022-04-27 20:59:26 +0900

    [#10] Ajax members 리펙토링 create 부분

--- ajax/membersAJAX.js
+++ ajax/membersAJAX.js
@@ -13,10 +13,24 @@ const nameText = queryString.get('input-text');
 
 let members;
 
-const membersSet = function(members) {
-  const membersSet = JSON.stringify(members);
-  sessionStorage.setItem('members', membersSet);
-  // window.location.reload();
+const ajax = function(method, url, data, callback) {
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function() {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      callback();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open(method, url, );
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send(data);
 };
 
 const membersCreate = function(form) {
@@ -31,23 +45,7 @@ const membersCreate = function(form) {
     memberAgeObject.value = '';
     membersRead();
   }
-  const xhrObject = new XMLHttpRequest();
-  xhrObject.onreadystatechange = function() {
-    if (xhrObject.readyState !== 4) return;
-    if (xhrObject.status === 200) {
-      successFunction();
-    } else {
-      const error = {
-        status: xhrObject.status,
-        statusText: xhrObject.statusText,
-        responseText: xhrObject.responseText
-      }
-      console.error(error);
-    }
-  };
-  xhrObject.open('POST', 'http://localhost:3100/api/v1/members');
-  xhrObject.setRequestHeader('Content-Type', 'application/json');
-  xhrObject.send(JSON.stringify(member));
+  ajax('POST', 'http://localhost:3100/api/v1/members', JSON.stringify(member), successFunction);
 };
 
 const membersRead = function() {
