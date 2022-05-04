3f046164bf63f813a2ef04e0f34173e2bf3307bf
abmysong <abmysong@gmail.com>
2022-05-04 20:51:55 +0900

    [#11] Axios 통신 delete 부분

--- ajax/membersAJAX.js
+++ ajax/membersAJAX.js
@@ -72,7 +72,7 @@ const membersRead = function() {
 
 const membersDelete = function(index) {
   const url = 'http://localhost:3100/api/v1/members/' + index;
-  ajax('DELETE', url, undefined, membersRead);
+  axios.delete(url).then(membersRead);
 };
 
 const membersUpdate = function(index) {
